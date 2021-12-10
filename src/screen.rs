eaebcc2ba94423950040ff08aa01277b6056df45
ShinyJonny <shinyjonnyuser@gmail.com>
2021-12-10 18:09:13 +0000

    Screen; Widget: cleaned api; changed types of dimensions to usize

--- src/screen.rs
+++ src/screen.rs
@@ -5,18 +5,18 @@ use std::rc::Rc;
 
 pub struct Screen {
     buffer: Vec<Vec<char>>,
-    height: u32,
-    width: u32,
-    widgets: Vec<WidgetHandle>,
+    height: usize,
+    width: usize,
+    widgets: Vec<Widget>,
 }
 
 impl Screen {
-    pub fn new(rows: u32, cols: u32) -> Self
+    pub fn new(rows: usize, cols: usize) -> Self
     {
         Self {
-            buffer: vec![vec![' '; cols as usize]; rows as usize],
-            height: rows as u32,
-            width: cols as u32,
+            buffer: vec![vec![' '; cols]; rows],
+            height: rows,
+            width: cols,
             widgets: Vec::new(),
         }
     }
@@ -27,16 +27,13 @@ impl Screen {
     {
         self.widgets.sort();
 
-        for row in self.buffer.iter_mut()
-        {
-            for c in row
-            {
+        for row in self.buffer.iter_mut() {
+            for c in row {
                 *c = ' ';
             }
         }
 
-        for i in 0..self.widgets.len()
-        {
+        for i in 0..self.widgets.len() {
             if self.widgets[i].borrow().has_border {
                 self.draw_border(i);
             }
@@ -47,19 +44,17 @@ impl Screen {
 
     pub fn refresh(&self)
     {
-        for row in &self.buffer
-        {
-            for c in row
-            {
+        for row in &self.buffer {
+            for c in row {
                 print!("{}", c);
             }
             println!();
         }
     }
 
-    pub fn add_widget(&mut self, start_y: u32, start_x: u32, height: u32, width: u32) -> WidgetHandle
+    pub fn add_widget(&mut self, start_y: u32, start_x: u32, height: usize, width: usize) -> Widget
     {
-        let w = WidgetHandle::new(start_y, start_x, height, width);
+        let w = Widget::new(start_y, start_x, height, width);
         self.widgets.push(w.clone());
         w
     }
@@ -75,15 +70,13 @@ impl Screen {
         let border_chars = w.border_style;
 
         if border_chars.0 != '\0' {
-            for i in 0..(width - 1)
-            {
+            for i in 0..(width - 1) {
                 self.buffer[start_y][start_x + i] = border_chars.0;
                 self.buffer[start_y + height - 1][start_x + i] = border_chars.0;
             }
         }
         if border_chars.1 != '\0' {
-            for i in 0..(height - 1)
-            {
+            for i in 0..(height - 1) {
                 self.buffer[start_y + i][start_x] = border_chars.1;
                 self.buffer[start_y + i][start_x + width - 1] = border_chars.1;
             }
@@ -122,10 +115,8 @@ impl Screen {
             start_y += 1;
         }
 
-        for y in 0..height
-        {
-            for x in 0..width
-            {
+        for y in 0..height {
+            for x in 0..width {
                 let c = w.buffer[y][x];
 
                 if c == '\0' {
@@ -138,17 +129,17 @@ impl Screen {
     }
 }
 
-pub struct WidgetHandle {
-    w: Rc<RefCell<Widget>>,
+pub struct Widget {
+    w: Rc<RefCell<InnerWidget>>,
 }
 
-impl WidgetHandle {
-    pub fn new(start_y: u32, start_x: u32, height: u32, width: u32) -> Self
+impl Widget {
+    pub fn new(start_y: u32, start_x: u32, height: usize, width: usize) -> Self
     {
         Self {
             w: Rc::new(RefCell::new(
-                Widget {
-                    buffer: vec![vec!['\0'; width as usize]; height as usize],
+                InnerWidget {
+                    buffer: vec![vec!['\0'; width]; height],
                     start_y,
                     start_x,
                     height,
@@ -202,12 +193,11 @@ impl WidgetHandle {
             return;
         }
 
-        if x >= width || y >= height {
+        if x as usize >= width || y as usize >= height {
             return;
         }
 
-        for (i, c) in line.chars().enumerate()
-        {
+        for (i, c) in line.chars().enumerate() {
             if x as usize + i >= width as usize {
                 break;
             }
@@ -218,18 +208,16 @@ impl WidgetHandle {
 
     pub fn clear(&mut self)
     {
-        for row in self.w.borrow_mut().buffer.iter_mut()
-        {
-            for c in row
-            {
+        for row in self.w.borrow_mut().buffer.iter_mut() {
+            for c in row {
                 *c = '\0';
             }
         }
     }
 }
 
-impl Deref for WidgetHandle {
-    type Target = Rc<RefCell<Widget>>;
+impl Deref for Widget {
+    type Target = Rc<RefCell<InnerWidget>>;
 
     fn deref(&self) -> &Self::Target
     {
@@ -237,19 +225,19 @@ impl Deref for WidgetHandle {
     }
 }
 
-impl Clone for WidgetHandle {
+impl Clone for Widget {
     fn clone(&self) -> Self
     {
-        WidgetHandle { w: self.w.clone() }
+        Widget { w: self.w.clone() }
     }
 }
 
-pub struct Widget {
+pub struct InnerWidget {
     buffer: Vec<Vec<char>>,
     start_y: u32,
     start_x: u32,
-    pub width: u32,
-    pub height: u32,
+    pub width: usize,
+    pub height: usize,
     z_index: u32,
     has_border: bool,
     border_style: (char, char, char, char, char, char),
@@ -258,28 +246,28 @@ pub struct Widget {
 // Widgets are sorted based on their z_index.
 // This simplifies the mechanisms for drawing or calculating which parts to draw.
 
-impl PartialOrd for WidgetHandle {
+impl PartialOrd for Widget {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering>
     {
         Some(self.w.borrow().z_index.cmp(&other.w.borrow().z_index))
     }
 }
 
-impl Ord for WidgetHandle {
+impl Ord for Widget {
     fn cmp(&self, other: &Self) -> Ordering
     {
         self.w.borrow().z_index.cmp(&other.w.borrow().z_index)
     }
 }
 
-impl PartialEq for WidgetHandle {
+impl PartialEq for Widget {
     fn eq(&self, other: &Self) -> bool
     {
         self.w.borrow().z_index == other.w.borrow().z_index
     }
 }
 
-impl Eq for WidgetHandle {}
+impl Eq for Widget {}
 
 
 #[cfg(test)]
@@ -289,8 +277,8 @@ mod tests {
     #[test]
     fn widget_handles_equal()
     {
-        let a = WidgetHandle::new(0, 0, 0, 0);
-        let b = WidgetHandle::new(0, 0, 0, 0);
+        let a = Widget::new(0, 0, 0, 0);
+        let b = Widget::new(0, 0, 0, 0);
         a.borrow_mut().z_index = 1;
         b.borrow_mut().z_index = 1;
 
@@ -300,8 +288,8 @@ mod tests {
     #[test]
     fn widget_handles_not_equal()
     {
-        let a = WidgetHandle::new(0, 0, 0, 0);
-        let b = WidgetHandle::new(0, 0, 0, 0);
+        let a = Widget::new(0, 0, 0, 0);
+        let b = Widget::new(0, 0, 0, 0);
         a.borrow_mut().z_index = 1;
         b.borrow_mut().z_index = 2;
 
@@ -311,8 +299,8 @@ mod tests {
     #[test]
     fn widget_handles_greater()
     {
-        let a = WidgetHandle::new(0, 0, 0, 0);
-        let b = WidgetHandle::new(0, 0, 0, 0);
+        let a = Widget::new(0, 0, 0, 0);
+        let b = Widget::new(0, 0, 0, 0);
         a.borrow_mut().z_index = 2;
         b.borrow_mut().z_index = 1;
 
@@ -322,8 +310,8 @@ mod tests {
     #[test]
     fn widget_handles_smaller_or_eq()
     {
-        let a = WidgetHandle::new(0, 0, 0, 0);
-        let b = WidgetHandle::new(0, 0, 0, 0);
+        let a = Widget::new(0, 0, 0, 0);
+        let b = Widget::new(0, 0, 0, 0);
         a.borrow_mut().z_index = 0;
         b.borrow_mut().z_index = 1;
 
@@ -333,13 +321,13 @@ mod tests {
     #[test]
     fn widget_handles_sort()
     {
-        let a = WidgetHandle::new(0, 0, 0, 0);
-        let b = WidgetHandle::new(0, 0, 0, 0);
-        let c = WidgetHandle::new(0, 0, 0, 0);
-        let d = WidgetHandle::new(0, 0, 0, 0);
-        let e = WidgetHandle::new(0, 0, 0, 0);
-        let f = WidgetHandle::new(0, 0, 0, 0);
-        let g = WidgetHandle::new(0, 0, 0, 0);
+        let a = Widget::new(0, 0, 0, 0);
+        let b = Widget::new(0, 0, 0, 0);
+        let c = Widget::new(0, 0, 0, 0);
+        let d = Widget::new(0, 0, 0, 0);
+        let e = Widget::new(0, 0, 0, 0);
+        let f = Widget::new(0, 0, 0, 0);
+        let g = Widget::new(0, 0, 0, 0);
         a.borrow_mut().z_index = 0;
         b.borrow_mut().z_index = 1;
         c.borrow_mut().z_index = 7;
@@ -353,8 +341,7 @@ mod tests {
 
         let mut last_z = 0;
 
-        for w in vector
-        {
+        for w in vector {
             assert!(last_z <= w.borrow().z_index);
             last_z = w.borrow_mut().z_index;
         }
